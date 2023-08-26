66294d4a0443a02c9169970e58884240fac7721c
Ani1952 <heroda02@gmail.com>
2023-08-26 17:52:24 +0530

    not much

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,93 +1,90 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const userInput = document.getElementById("input-number");
-  const operationButtons = document.querySelectorAll("#calc-actions button");
-  const currentResultOutput = document.getElementById("current-result");
-  const calculationLogsOutput = document.getElementById("current-log");
+const userInput = document.getElementById("input-number");
+const operationButtons = document.querySelectorAll("#calc-actions button");
+const currentResultOutput = document.getElementById("current-result");
+const calculationLogsOutput = document.getElementById("current-log");
 
-  let currentResult = 0;
+let currentResult = 0;
 
-  function outputResult(result) {
-    currentResultOutput.textContent = result;
-    calculationLogsOutput.textContent = makeLogOutputConsole();
-    userInput.value="";
-  }
+function outputResult(result) {
+  currentResultOutput.textContent = result;
+  calculationLogsOutput.textContent = makeLogOutputConsole();
+  userInput.value = "";
+}
 
-  const logs = [];
+const logs = [];
 
-  function addToLogs(operation, previousResult, newInputValue, result) {
-    const logEntry = {
-      operator: operation,
-      prevRes: previousResult,
-      inputValue: newInputValue,
-      res: result,
-    };
-    logs.push(logEntry);
-  }
+function addToLogs(operation, previousResult, newInputValue, result) {
+  const logEntry = {
+    operator: operation,
+    prevRes: previousResult,
+    inputValue: newInputValue,
+    res: result,
+  };
+  logs.push(logEntry);
+}
 
-  function makeLogOutputConsole() {
-    let allLogsOutput = '';
-  
-    for (let i = 0; i < logs.length; i++) {
-      const currentLog = logs[i];
-      let operatorSymbol =
-        currentLog.operator === '+'
-          ? '+'
-          : currentLog.operator === '-'
-          ? '-'
-          : currentLog.operator === '*'
-          ? '*'
-          : '/';
-      const logOutput = `${currentLog.prevRes} ${operatorSymbol} ${currentLog.inputValue} = ${currentLog.res}\n`;
-  
-      allLogsOutput += logOutput;
-    }
-  
-    return allLogsOutput;
-  }
+function makeLogOutputConsole() {
+  let allLogsOutput = "";
 
-
-  function performOperation(operator) {
-    const num = parseInt(userInput.value);
-    const operatorSymbol =
-      operator === "btn-add"
+  for (let i = 0; i < logs.length; i++) {
+    const currentLog = logs[i];
+    let operatorSymbol =
+      currentLog.operator === "+"
         ? "+"
-        : operator === "btn-subtract"
+        : currentLog.operator === "-"
         ? "-"
-        : operator === "btn-multiply"
+        : currentLog.operator === "*"
         ? "*"
         : "/";
-    const description = `${currentResult} ${operatorSymbol} ${num}`;
-    let initValue;
-    switch (operator) {
-      case "btn-add":
-        initValue = currentResult;
-        currentResult += num;
-        addToLogs("+", initValue, num, currentResult);
-        break;
-      case "btn-subtract":
-        initValue = currentResult;
-        currentResult -= num;
-        addToLogs("-", initValue, num, currentResult);
-        break;
-      case "btn-multiply":
-        initValue = currentResult;
-        currentResult *= num;
-        addToLogs("*", initValue, num, currentResult);
-        break;
-      case "btn-divide":
-        initValue = currentResult;
-        currentResult /= num;
-        addToLogs("/", initValue, num, currentResult);
-        break;
-    }
+    const logOutput = `${currentLog.prevRes} ${operatorSymbol} ${currentLog.inputValue} = ${currentLog.res}\n`;
 
-    outputResult(currentResult);
+    allLogsOutput += logOutput;
   }
 
-  operationButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      performOperation(button.id);
-    });
+  return allLogsOutput;
+}
+
+function performOperation(operator) {
+  const num = parseInt(userInput.value);
+  const operatorSymbol =
+    operator === "btn-add"
+      ? "+"
+      : operator === "btn-subtract"
+      ? "-"
+      : operator === "btn-multiply"
+      ? "*"
+      : "/";
+  const description = `${currentResult} ${operatorSymbol} ${num}`;
+  let initValue;
+  switch (operator) {
+    case "btn-add":
+      initValue = currentResult;
+      currentResult += num;
+      addToLogs("+", initValue, num, currentResult);
+      break;
+    case "btn-subtract":
+      initValue = currentResult;
+      currentResult -= num;
+      addToLogs("-", initValue, num, currentResult);
+      break;
+    case "btn-multiply":
+      initValue = currentResult;
+      currentResult *= num;
+      addToLogs("*", initValue, num, currentResult);
+      break;
+    case "btn-divide":
+      initValue = currentResult;
+      currentResult /= num;
+      addToLogs("/", initValue, num, currentResult);
+      break;
+  }
+
+  outputResult(currentResult);
+}
+
+operationButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    performOperation(button.id);
   });
-  console.log(logs);
 });
+console.log(logs);
